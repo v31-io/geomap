0ea14a95ef7bd172d64b7adf45071102d826942c
sidxdev <sidsriv93@gmail.com>
2025-04-28 08:31:51 +0000

    - diskcache interval table - get_valid_ids

--- python/lib/glad.py
+++ python/lib/glad.py
@@ -1,12 +1,18 @@
+import numpy as np
 import pandas as pd
+from datetime import datetime, timedelta
+from diskcache import Cache
 
 
 class GLAD():
   '''
   https://glad.umd.edu/ard/home
   '''
+
+  _cache = Cache(__name__)
   _base_url = "https://glad.umd.edu/"
   _interval_id_url = f'{_base_url}/users/Potapov/ARD/16d_intervals.xlsx'
+  _days_before_update = 20
   
   def __init__(self):
     self.get_interval_table()
@@ -15,16 +21,42 @@ class GLAD():
     '''
     Get the 16 day interval table from GLAD.
     '''
-    if not hasattr(self, '_interval_table'):
+    if not 'interval_table' in self._cache:
       print('Downloading interval table from GLAD...')
+      # Interval ID table
       self._interval_table = pd.read_excel(self._interval_id_url, sheet_name='16d interval ID', 
                                             header=1, index_col='Year')
-      interval_dates = pd.read_excel(self._interval_id_url, sheet_name='16d interval dates',
+      # Date lookup table
+      dates = pd.read_excel(self._interval_id_url, sheet_name='16d interval dates',
                                             index_col='Interval ID')['end Date'].to_list()
       
+      # Use the lookup table to create a date table
       self._interval_dates = self._interval_table.copy().astype('str')
       for index, _ in self._interval_dates.iterrows():
-        self._interval_dates.loc[index] = interval_dates.copy()
+        self._interval_dates.loc[index] = dates.copy()
       self._interval_dates = self._interval_dates.astype('datetime64[ns]')
+      for index, _ in self._interval_dates.iterrows():
+        self._interval_dates.loc[index] = self._interval_dates.loc[index].apply(lambda x: datetime(index, x.month, x.day))
+
+      self._cache.set('interval_table', self._interval_table)
+      self._cache.set('interval_dates', self._interval_dates)
+
+    else:
+      print('Loading interval table from cache...')
+      self._interval_table = self._cache.get('interval_table')
+      self._interval_dates = self._cache.get('interval_dates')
                                             
     return self._interval_table, self._interval_dates
+  
+  def get_valid_ids(self):
+    '''
+    Get the valid interval IDs from GLAD which are before the current date minus `_days_before_update`.
+    '''
+    interval_table, interval_dates = self.get_interval_table()
+
+    today = datetime.now() - timedelta(days=self._days_before_update)
+
+    interval_table = interval_table[interval_dates < today]
+    ids = interval_table.to_numpy().flatten()
+    ids = ids[~(np.isnan(ids))].astype(int).tolist()
+    return ids

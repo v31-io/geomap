4488372b0b8c7622bb858533b32c7cd0a9db0323
sidxdev <sidsriv93@gmail.com>
2025-05-06 15:06:33 +0000

    keycloak api

--- python/api/main.py
+++ python/api/main.py
@@ -1,8 +1,9 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi_utils.tasks import repeat_every
 
 from api.tasks.update_tiles import update_tiles
+from api.services.keycloak import TokenVerifier
 
 
 app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
@@ -10,7 +11,7 @@ app.state.tile_cache = {}
 
 # Tasks
 @app.on_event("startup")
-@repeat_every(seconds=3600)
+@repeat_every(seconds=86400)
 def task_update_tiles() -> None:
     app.state.tile_cache = update_tiles()
 
@@ -20,7 +21,7 @@ def head_root():
   return {"status": "OK"}
 
 @app.get("/")
-def get_root():
+def get_root(_: dict = Depends(TokenVerifier(roles=['access']))):
   return app.state.tile_cache
 
 if __name__ == "__main__":

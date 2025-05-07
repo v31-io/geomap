b061b3262f296fbac34a0e889fa14409d5d3251d
sidxdev <sidsriv93@gmail.com>
2025-05-07 17:58:26 +0000

    remove rgba from ingest

--- python/worker/ingest_glad_ard_tile.py
+++ python/worker/ingest_glad_ard_tile.py
@@ -20,6 +20,5 @@ print(f'{len(valid_ids)} IDs found for ingestion.')
 for id in tqdm(valid_ids):
   try:
     glad.get_image(tile_id=tile_id, interval_id=id)
-    glad.get_image_rgba(tile_id=tile_id, interval_id=id)
   except Exception as e:
     print(f'Failed with error - {e}')
